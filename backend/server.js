103255c179af32940584ec25c06a098dada4c5a2
saxena100parth <87128985+saxena100parth@users.noreply.github.com>
2023-08-11 13:27:44 +0530

    production

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,13 +17,24 @@ connectDB();
 const app = express();
 app.use(express.json()); // to accept the json data
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+// -----------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname1, "../frontend", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+// -----------------  //
 app.use(notFound);
 app.use(errorHandler);
 
